7bd9405a74f19e7d56d9ab97e1022372daa14929
Brandon Amos <bdamos@vt.edu>
2015-10-29 07:50:15 -0400

    Fix variable type for #40.

--- util/prune-dataset.py
+++ util/prune-dataset.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('inPlaceDir', type=str,
                         help="Directory to prune in-place.")
-    parser.add_argument('--numImagesThreshold', type=str,
+    parser.add_argument('--numImagesThreshold', type=int,
                         help="Delete directories with less than this many images.",
                         default=10)
     args = parser.parse_args()
