f7baa6fa9af36e3b8a1199d8c0a3c0ef61d80028
Phillip Schanely <phil@schanely.com>
2015-10-30 12:42:49 -0400

    linting errors

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setup(name='openface',
       version='0.1.1',
       description="Face recognition with Google's FaceNet deep neural network.",
       url='https://github.com/yarnbasket/openface',
-      packages=['openface','openface.alignment'],
+      packages=['openface', 'openface.alignment'],
       package_data={'openface': ['*.lua']},
-  )
+)
