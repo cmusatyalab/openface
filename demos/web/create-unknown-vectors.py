c8a4f340954cdae73c54b9826abf5a23774d8853
Brandon Amos <bamos@cs.cmu.edu>
2015-12-11 15:55:56 -0500

    Untested improvements to create-unknown-vectors.

--- demos/web/create-unknown-vectors.py
+++ demos/web/create-unknown-vectors.py
@@ -29,30 +29,47 @@ import openface
 from openface.alignment import NaiveDlib
 from openface.data import iterImgs
 
+parser = argparse.ArgumentParser()
+parser.add_argument('imgDir', type=str, help="Input image directory.")
+parser.add_argument('--numImages', type=int, default=1000)
+parser.add_argument('--model', type=str, help="TODO",
+                    default="./models/openface/nn4.v1.t7")
+parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",
+                    default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+parser.add_argument('--outputFile', type=str,
+                    help="Output file, stored in numpy serialized format.",
+                    default="./unknown.npy")
+parser.add_argument('--imgDim', type=int, help="Default image size.",
+                    default=96)
+args = parser.parse_args()
+
+align = NaiveDlib(args.dlibFacePredictor)
+openface = openface.TorchWrap(args.model, imgDim=args.imgDim, cuda=False)
+
+def getRep(imgPath):
+    bgrImg = cv2.imread(imgPath)
+    if bgrImg is None:
+        return None
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+
+    bb = align.getLargestFaceBoundingBox(rgbImg)
+    if bb is None:
+        return None
+
+    alignedFace = align.alignImg("affine", args.imgDim, rgbImg, bb)
+    if alignedFace is None:
+        return None
+
+    rep = net.forwardImage(alignedFace)
+    return rep
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('imgDir', type=str, help="Input image directory.")
-    parser.add_argument('--numImages', type=int, default=1000)
-    parser.add_argument('--model', type=str, help="TODO",
-                        default="./models/openface/nn4.v1.t7")
-    parser.add_argument('--outputFile', type=str,
-                        help="Output file, stored in numpy serialized format.",
-                        default="./unknown.npy")
-    parser.add_argument('--imgDim', type=int, help="Default image size.",
-                        default=96)
-    args = parser.parse_args()
-
-    align = NaiveDlib("models/dlib/",
-                      "shape_predictor_68_face_landmarks.dat")
-    openface = openface.TorchWrap(args.model, imgDim=args.imgDim, cuda=False)
-
     allImgs = list(iterImgs(args.imgDir))
     imgObjs = random.sample(allImgs, args.numImages)
 
     reps = []
     for imgObj in imgObjs:
-        rep = openface.forward(imgObj.path)
-        rep = np.array(rep)
+        rep = getRep(imgObj.path)
         reps.append(rep)
 
     np.save(args.outputFile, np.row_stack(reps))
