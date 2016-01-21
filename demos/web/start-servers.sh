19d9d8c703c40054874ae7daf8e882312de85ced
Brandon Amos <bamos@cs.cmu.edu>
2016-01-20 23:18:17 -0500

    Add more details to start-servers.sh
    Thanks @justinsteele

--- demos/web/start-servers.sh
+++ demos/web/start-servers.sh
@@ -20,8 +20,27 @@ esac
 cd $(dirname $0)
 trap 'kill $(jobs -p)' EXIT
 
-printf "HTTP Port: %s\n" $HTTP_PORT
-printf "WebSocket Port: %s\n\n" $WEBSOCKET_PORT
+cat <<EOF
+
+Starting the HTTP server on port $HTTP_PORT
+and the WebSocket server on port $WEBSOCKET_PORT.
+
+Access the demo through the HTTP server in your browser.
+If you're running on the same computer outside of Docker, use http://localhost:$HTTP_PORT
+If you're running on the same computer with Docker, find the IP
+address of the Docker container and use http://<docker-ip>:$HTTP_PORT.
+If you're running on a remote computer, find the IP address
+and use http://<remote-ip>:$HTTP_PORT.
+
+WARNING: Chromium refuses to connect to the insecure WebSocket server
+if you are running a remote or Docker deployment.
+We have posted a workaround to forward traffic through localhost
+using ncat at http://cmusatyalab.github.io/openface/demo-1-web/.
+Track our progress on fixing this at:
+https://github.com/cmusatyalab/openface/issues/75.
+
+
+EOF
 
 WEBSOCKET_LOG='/tmp/openface.websocket.log'
 printf "WebSocket Server: Logging to '%s'\n\n" $WEBSOCKET_LOG
