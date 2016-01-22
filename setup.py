6d69373a442f6afc7ebf1a861eab90c84244adc5
Brandon Amos <bamos@cs.cmu.edu>
2016-01-22 12:03:36 -0500

    setup.py: Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='openface',
-    version='0.1.1',
+    version='0.2.0',
     description="Face recognition with Google's FaceNet deep neural network.",
     url='https://github.com/cmusatyalab/openface',
     packages=['openface'],
