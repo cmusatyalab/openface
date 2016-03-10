d4ee384d5e103a44f7732bec158dab944639082b
Brandon Amos <bdamos@vt.edu>
2016-03-09 22:26:12 -0500

    demos/vis-outputs: Add aligned example images, fix image.load usage, and output where the images are being saved to
    Thanks @pppoe! https://github.com/cmusatyalab/openface/pull/107

--- demos/vis-outputs.lua
+++ demos/vis-outputs.lua
@@ -28,10 +28,10 @@ cmd:text('Visualize OpenFace outputs.')
 cmd:text()
 cmd:text('Options:')
 
-cmd:option('-imgPath', 'images/examples-aligned/examples/lennon-1.png',
+cmd:option('-imgPath', 'images/examples-aligned/lennon-1.png',
            'Path to aligned image.')
 cmd:option('-filterOutput',
-           'images/examples-aligned/examples/lennon-1',
+           'images/examples-aligned/lennon-1',
            'Output directory.')
 cmd:option('-model', './models/openface/nn4.small2.v1.t7', 'Path to model.')
 cmd:option('-imgDim', 96, 'Image dimension. nn1=224, nn4=96')
@@ -53,11 +53,13 @@ net:evaluate()
 print(net)
 
 local img = torch.Tensor(1, 3, opt.imgDim, opt.imgDim)
-img[1] = image.load(opt.imgPath, opt.imgDim)
-img[1] = image.scale(img[1], opt.imgDim, opt.imgDim)
+local img_orig = image.load(opt.imgPath, 3)
+img[1] = image.scale(img_orig, opt.imgDim, opt.imgDim)
 net:forward(img)
 
-f, err = io.open(opt.filterOutput .. '/preview.html', 'w')
+local fName = opt.filterOutput .. '/preview.html'
+print("Outputting filter preview to '" .. fName .. "'")
+f, err = io.open(fName, 'w')
 if err then
    print("Error: Unable to open preview.html");
    os.exit(-1)
