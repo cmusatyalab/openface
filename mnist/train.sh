1ee7d017dc4f243c512d514a5c021c2de918fc33
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2016-11-30 08:00:21 +0300

    sh s changed

--- mnist/train.sh
+++ mnist/train.sh
@@ -11,5 +11,5 @@ fi
 cd ../training
 
 th main.lua -data $RAW_DIR/training -modelDef $WORK_DIR/model/nn4.small2.def.lua -cache $WORK_DIR/data/cache  \
- -save $RESULT_DIR  -nDonkeys 2  -cuda -cudnn -peoplePerBatch 5 -imagesPerPerson 5 -testPy ../evaluation/classify.py \
+ -save $RESULT_DIR  -nDonkeys 2  -cuda -cudnn -peoplePerBatch 10 -imagesPerPerson 15 -testPy ../evaluation/classify.py \
  -testDir $RAW_DIR/testing -testBatchSize 100 -epochSize 250 -nEpochs 50 -imgDim 28 -channelSize 1
