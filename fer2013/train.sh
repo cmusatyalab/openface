7235503501b471f4097faea94858cc7f2e85f66d
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2016-12-01 23:51:49 +0300

    bug fix

--- fer2013/train.sh
+++ fer2013/train.sh
@@ -9,8 +9,8 @@ if [ ! -d $RAW_DIR ]; then
     python ../util/csv_to_image.py --inputFile fer2013.csv --outputDir $RAW_DIR
 fi
 if [ ! -d $ALIGNED_DIR/train ]; then
-    python ../util/align-dlib.py $RAW_DIR/train align  outerEyesAndNose $ALIGNED_DIR/train --size 48 --rgb 0 \
-        --fallbackLfw $RAW_DIR/train
+    python ../util/align-dlib.py $RAW_DIR/training align  outerEyesAndNose $ALIGNED_DIR/training --size 48 --rgb 0 \
+        --fallbackLfw $RAW_DIR/training
 fi
 if [ ! -d $ALIGNED_DIR/publictest ]; then
     python ../util/align-dlib.py $RAW_DIR/publictest align  outerEyesAndNose $ALIGNED_DIR/publictest --size 48 --rgb 0 \
@@ -25,8 +25,8 @@ fi
 
 cd ../training
 
-th main.lua -data $ALIGNED_DIR/train -modelDef $WORK_DIR/model/nn4.small2.def.lua -cache $WORK_DIR/data/cache  \
- -save $RESULT_DIR  -nDonkeys 2  -cuda -cudnn -peoplePerBatch 7 -imagesPerPerson 5 -testPy ../evaluation/classify.py \
+th main.lua -data $ALIGNED_DIR/training -modelDef $WORK_DIR/model/nn4.small2.def.lua -cache $WORK_DIR/data/cache  \
+ -save $RESULT_DIR  -nDonkeys 2  -cuda -cudnn -peoplePerBatch 7 -imagesPerPerson 15 -testPy ../evaluation/classify.py \
  -testDir $ALIGNED_DIR/publictest -testBatchSize 100 -epochSize 100 -nEpochs 50 -imgDim 48 -channelSize 1
 
 
