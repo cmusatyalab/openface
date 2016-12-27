11502e91ea7256cf6f3a0a016a13412bd6171cd5
bau_fbe <cenk.math@gmail.com>
2016-12-27 17:59:25 +0300

    test scores added

--- run_all.sh
+++ run_all.sh
@@ -7,23 +7,32 @@ HERE=$PWD
 cd $HERE/cife
 sh train.sh
 
+cd $HERE/training/
+th main.lua -data /home/bau_fbe/Documents/openface-v2/gamo/data/aligned/train \
+    -modelDef /home/bau_fbe/Documents/openface-v2/models/mine/nn4.small2.def.64_1.lua \
+    -cache /home/bau_fbe/Documents/openface-v2/gamo/data/cache \
+    -save /home/bau_fbe/Documents/openface-v2/gamo/data/results_triplet/nn4.small2/ \
+     -nDonkeys 2  -peoplePerBatch 7 -imagesPerPerson 25 -testing -epochSize 500 -nEpochs 170 -imgDim 64 \
+     -criterion triplet -retrain /home/bau_fbe/Documents/openface-v2/gamo/data/results_triplet/nn4.small2/model_80.t7  -epochNumber 81
+
+cd $HERE/gamo
+sh train.sh
+
+cd $HERE/fer2013
+sh train.sh
+
+
+cd $HERE/cifar10
+sh train.sh
+
+cd $HERE/gamo
+sh test.sh
+
 cd $HERE/cife
 sh test.sh
 
 cd $HERE/fer2013
-sh train.sh
-
-cd $HERE/fer2013
 sh test.sh
-
-cd $HERE/gamo
-sh train.sh
-
-cd $HERE/gamo
-sh test.sh
-
-cd $HERE/cifar10
-sh train.sh
 
 cd $HERE/cifar10
 sh test.sh
