a76dd7a92cbde3ff05f055086d400bb2d50f0f22
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2017-01-07 15:09:46 +0300

    update classify

--- cifar10/train.sh
+++ cifar10/train.sh
@@ -7,7 +7,7 @@ train ()
 {
     if [ ! -f $2/model_1.t7 ]; then
         th main.lua -data $ALIGNED_DIR/train -modelDef $1 -cache $WORK_DIR/data/cache  \
-            -save $2  -nDonkeys 10  -peoplePerBatch 2 -imagesPerPerson $4 -testing \
+            -save $2  -nDonkeys 10  -peoplePerBatch 10 -imagesPerPerson $4 -testing \
             -epochSize 500 -nEpochs 1 -criterion $3 -imgDim 32 -cuda
     fi
 }
