3f64481ddf92b732e8fb23b03509466c08f8ef6e
Cenk Bircanoglu <cenk.bircanoglu@gmail.com>
2017-03-06 22:39:21 +0300

    kldiv added

--- training/garbage/testKLDiv.lua
+++ training/garbage/testKLDiv.lua
@@ -12,6 +12,7 @@ cuda = false
 
 require 'nn'
 require '../loss/KLDivergence'
+require '../loss/KLDivergence2'
 torch.setdefaulttensortype('torch.FloatTensor')
 if cuda then
     require 'cutorch'
@@ -24,16 +25,67 @@ local b = colour.blue
 
 torch.manualSeed(0)
 
-x =  torch.Tensor { { 0.1, 0.2 }, { 0.3, 0.4 }, { 0.5, 0.6 }, { 0.7, 0.8 }, { 0.9, 0.95 }, { 0.975, 0.99 } }
-print(b('ancore embedding batch:')); print(x)
+x = torch.Tensor { { 0.1, 0.2 }, { 0.3, 0.4 }, { 0.5, 0.6 }, { 0.5, 0.7 } }
+y = torch.Tensor { 1, 1, 2, 2 }
 
-y = torch.Tensor { 1, 1, 1, 2, 2, 2 }
-print(b('positive embedding batch:')); print(y)
-
--- Testing the loss function forward and backward
-loss = nn.BatchKLDivCriterion()
+loss = nn.BatchKLDivCriterion2()
 if cuda then loss = loss:cuda() end
 print(colour.red('loss: '), loss:forward(x, y), '\n')
 gradInput = loss:backward(x, y)
-print(b('gradInput[1]:')); print(gradInput[1])
+print(b('gradInput[1]:')); print(gradInput)
 
+function findClassId(i, numberPerClass)
+    local id_ = 1
+    local ind = math.floor((i - 1) / numberPerClass)
+    id_ = id_ + ind
+    return id_
+end
+
+function pairss(embeddings, numPerClass)
+    local a1s_table = {}
+    local a2s_table = {}
+    local targets_table = {}
+    local mapper = {}
+    for i = 1, embeddings:size(1) do
+        local classIdi = findClassId(i, numPerClass)
+        for j = 1, embeddings:size(1) do
+            if i ~= j then
+                local classIdj = findClassId(j, numPerClass)
+                table.insert(a1s_table, embeddings[i])
+                table.insert(a2s_table, embeddings[j])
+
+                local target
+                if classIdi == classIdj then
+                    target = 1
+                else
+                    target = 0
+                end
+                table.insert(targets_table, target)
+                table.insert(mapper, { i, j })
+            end
+        end
+    end
+    local a1s = torch.cat(a1s_table):view(table.getn(a1s_table), 2)
+    local a2s = torch.cat(a2s_table):view(table.getn(a2s_table), 2)
+
+    local targets = torch.Tensor(targets_table)
+
+
+    local as = { a1s, a2s }
+
+    return as, targets, mapper
+end
+
+local ass, targets, mapper = pairss(x, 2)
+loss = nn.BatchKLDivCriterion()
+if cuda then loss = loss:cuda() end
+print(colour.red('loss: '), loss:forward(ass, targets), '\n')
+gradInput = loss:backward(ass, targets)
+
+local gradient_all = torch.Tensor(x:size(1), ass[1]:size(2)):type(x:type())
+
+for i = 1, table.getn(mapper) do
+    gradient_all[mapper[i][1]]:add(gradInput[i])
+end
+
+print(gradient_all)
--- training/loss/KLDivergence.lua
+++ training/loss/KLDivergence.lua
@@ -5,169 +5,32 @@ local epsilon = 1e-35 --to avoid calculating log(0) or dividing 0
 
 function BatchKLDivCriterion:__init(margin)
     parent.__init(self)
-    self.sizeAverage = false
     self.margin = margin or 2 -- 2 is the empirical value for mnist
+    self.Li = nil
 end
 
-function BatchKLDivCriterion:KLDiv(target, input)
-    -- The function calculates the KL-divergence
-    -- target and input must be dim==1
-    self.buf_t = self.buf_t or target.new()
-    self.buf_t:resizeAs(target)
-    self.buf_i = self.buf_i or input.new()
-    self.buf_i:resizeAs(input)
 
-    -- KL(T||I) = \sum T(logT-logI)
-    self.buf_t = target + epsilon
-    self.buf_i = input + epsilon
-    self.buf_t:log() -- logT
-    self.buf_i:log() -- logI
-    self.buf_t = self.buf_t - self.buf_i --logT-logI
-    local dist = self.buf_t:cmul(target):sum() -- a number
+function BatchKLDivCriterion:updateOutput(inputs, target)
 
-    return dist
+    local p = inputs[1]
+    local q = inputs[2]
+
+    local N = target:size(1)
+    local KLDivQP = (torch.log(q + epsilon) - torch.log(p + epsilon)):cmul(q)
+
+    self.Li = (torch.sum(KLDivQP, 2)):cmul(target) + torch.max(torch.cat(torch.Tensor(N):zero():type(torch.type(KLDivQP)), self.margin - torch.sum(KLDivQP, 2), 2), 2):cmul(1 - target)
+
+    return self.Li:sum() * 2 / N
 end
 
-function BatchKLDivCriterion:is_pair_labeled(i, j, target)
-    if target:dim() == 2 then
-        return target[i][j] ~= 0
-    end
-    return target[i] > 0 and target[j] > 0
-end
+function BatchKLDivCriterion:updateGradInput(inputs, target)
 
-function BatchKLDivCriterion:is_simi_pair(i, j, target)
-    if target:dim() == 2 then
-        return target[i][j] == 1
-    end
-    return target[i] == target[j]
-end
+    local p = inputs[1]
+    local q = inputs[2]
+    local N = target:size(1)
 
-function BatchKLDivCriterion:loss(p, q, input, simi, cache_idx, load_cache)
-    cache_idx = cache_idx or 0
-    load_cache = load_cache or false
-    if load_cache then return self.loss_cache[cache_idx] end --read cache to speedup
-
-    local loss = self:KLDiv(input[p], input[q])
-    if not simi then --hinge lost
-        loss = self.margin - loss
-        if loss < 0 then loss = 0 end
-    end
-    self.loss_cache[cache_idx] = loss --save the loss to cache
-    return loss
-end
-
-function BatchKLDivCriterion:updateOutput(input, target)
-    -- Unlabeled data is handled: target[i]=0 means no label for sample i
-    -- assert(input:dim() == 2)
-    -- assert(input:size(1) > 1)
-    self.output = 0
-    local pair_count = 0
-    local pair_idx = 0
-
-    -- prepare loss_cache buffer for saving some computation
-    if target:dim() == 1 then
-        pair_count = target:size(1) * (target:size(1) + 1) / 2 - target:size(1)
-    elseif target:size(1) == target:size(2) then
-        pair_count = target:ne(0):sum() / 2
-    else
-        pair_count = target:size(1)
-    end
-    self.loss_cache = self.loss_cache or input[1].new()
-    self.loss_cache:resize(pair_count * 2):fill(0) -- for saving the loss of both direction
-
-    -- We handle 3 types of 'target':
-    -- 1. nx1 class label - so we could easily replace CrossEntropyCriterion by BatchKLDivCriterion
-    -- 2. nxn relationship matrix
-    -- 3. nx3 tuple
-
-    -- target is nx1 class label or nxn relationship matrix
-    if target:dim() == 1 or target:size(1) == target:size(2) then
-        for i = 1, input:size(1) do
-            for j = 1, input:size(1) do
-                if i ~= j and self:is_pair_labeled(i, j, target) then
-                    pair_idx = pair_idx + 1
-                    self.output = self.output + self:loss(j, i, input, self:is_simi_pair(i, j, target), pair_idx)
-                end
-            end
-        end
-        -- target is nx3: (i,j,relationship); relationship= 1:similar pair, 0: dissimilar pair
-    else
-        local test_cost = 0
-        for p = 1, pair_count do
-            local i, j, s = target[p][1], target[p][2], target[p][3]
-            s = s == 1
-            self.output = self.output + self:loss(j, i, input, s, p) + self:loss(i, j, input, s, pair_count + p)
-        end
-    end
-
-    if pair_count > 0 then
-        self.output = self.output / pair_count
-    end
-
-    return self.output
-end
-
-function BatchKLDivCriterion:updateGradInput(input, target)
-    --assert(input:dim() == 2)
-    --assert(input:size(1) > 1)
-    local pair_count = 0
-    local pair_idx = 0
-    local factor = 0 -- Indicator of similar pair (similar: 1; dissimilar: -1)
-    self.gradInput = self.gradInput or input.new()
-    if self.gradInput:type() ~= input:type() then self.gradInput = input.new() end
-    self.gradInput:resizeAs(input):fill(0)
-    self.buf = self.buf or input[1].new()
-    self.buf:resizeAs(input[1])
-
-    if target:dim() == 1 then
-        pair_count = target:size(1) * (target:size(1) + 1) / 2 - target:size(1)
-    elseif target:size(1) == target:size(2) then
-        pair_count = target:ne(0):sum() / 2
-    else
-        pair_count = target:size(1)
-    end
-
-    -- target is nx1 class label or nxn relationship matrix
-    if target:dim() == 1 or target:size(1) == target:size(2) then
-        for i = 1, input:size(1) do
-            for j = 1, input:size(1) do
-                if i ~= j and self:is_pair_labeled(i, j, target) then
-                    pair_idx = pair_idx + 1
-                    if self:is_simi_pair(i, j, target) then factor = 1 else factor = -1 end
-                    local loss = self:loss(j, i, input, factor == 1, pair_idx, true)
-                    if loss ~= 0 then
-                        self.buf:copy(input[j]):cdiv(input[i] + epsilon):mul(-1)
-                        self.gradInput[i] = self.gradInput[i] + self.buf:mul(factor)
-                    end
-                end
-            end
-        end
-        -- target is nx3: (i,j,relationship); relationship= 1:similar pair, 0: dissimilar pair
-    else
-        for p = 1, pair_count do
-            local i, j, s = target[p][1], target[p][2], target[p][3]
-            s = s == 1
-            local loss_i = self:loss(j, i, input, s, p, true) --read the loss from cache
-            local loss_j = self:loss(i, j, input, s, pair_count + p, true)
-            if s then factor = 1 else factor = -1 end
-            -- Implementation trick: Ignore the gradient when loss=0 (even it is a similar pair).
-            -- The trick saves some computation and doesn't affect the result.
-            -- Do the clustering multiple times with random initialization is the key to reach lower training error.
-            -- dloss(p,q)/dq_i = -p_i/q_i
-            if loss_i ~= 0 then
-                self.buf:copy(input[j]):cdiv(input[i] + epsilon):mul(-1)
-                self.gradInput[i] = self.gradInput[i] + self.buf:mul(factor)
-            end
-            -- dloss(q,p)/dp_i = -q_i/p_i
-            if loss_j ~= 0 then
-                self.buf:copy(input[i]):cdiv(input[j] + epsilon):mul(-1)
-                self.gradInput[j] = self.gradInput[j] + self.buf:mul(factor)
-            end
-        end
-    end
-
-    -- average by size
-    if self.sizeAverage then self.gradInput = self.gradInput:div(pair_count) end
-
+    local pdivq = q:cdiv(p + epsilon):mul(-1)
+    local li = torch.cmul(pdivq, self.Li:gt(0):repeatTensor(p:size(2), 1):t():type(p:type()))
+    self.gradInput = torch.cmul(li, target:repeatTensor(p:size(2), 1):t():type(p:type())) + torch.cmul(li, (target - 1):repeatTensor(p:size(2), 1):t():type(p:type()))
     return self.gradInput
 end
